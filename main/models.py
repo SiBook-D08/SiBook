7dbe6363e0c94c6c251cdb076e7ee2842f7f205b
Adrian <adrian.hamzah15@gmail.com>
2023-10-12 23:57:10 +0700

    login and register added

--- main/models.py
+++ main/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-
-class Book(models.Model):
-    title = models.CharField(max_length=255)
-    publisher = models.CharField(max_length=255)
-    publishDate = models.CharField(max_length=255)
-    description = models.CharField(max_length=2000)
-    page_count = models.IntegerField()
--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,8 @@ from main.views import *
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main')
+    path('', show_main, name='show_main'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
