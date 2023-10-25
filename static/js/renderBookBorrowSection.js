f94ca666fb57c4b5711600c5d08160819dc77969
Adrian <adrian.hamzah15@gmail.com>
2023-10-25 23:51:11 +0700

    doing collectstatic

--- static/js/renderBookBorrowSection.js
+++ static/js/renderBookBorrowSection.js
@@ -11,14 +11,12 @@ async function displayProductsBorrowed(products){
         htmlString += `\n<div class="col-lg-4 col-md-6 mb-4">
             <div id="${bookData.pk}" class="card h-100">
                 <div class="card-header-borrowed d-flex justify-content-between align-items-center">
-                    <h5 class="card-title mb-0">${bookData.fields.title}</h5>
+                    <h5 class="card-title mb-0" style="background-color:red; border-radius: 5px;"> UNAVAILABLE</h5>
                 </div>
-                <div class="card-body-borrowed rounded-lg">
-					<p class="text-muted small">Id: ${bookData.pk}</p>
-                    <p class="text-muted small">Peminjam: ${userData.fields.username}</p>
-                    <p class="text-muted small">Penulis: ${bookData.fields.author}</p>
-                    <p class="text-muted small">Banyak Halaman: ${bookData.fields.num_pages}</p>
-                    <p class="card-text">${bookData.fields.description}</p>
+                <div class="card-body-borrowed rounded-lg" style="text-align:center;">	
+                    <strong> <p class="card-text">Peminjam: ${userData.fields.username}</p> </strong>
+                    <strong> <p class="card-text">${bookData.fields.title}</p> </strong>
+                    <strong> <p class="card-text">by: ${bookData.fields.author}</p> </strong>
                 </div>
                 <div class = card-footer-borrowed > </div>
             </div>
@@ -29,20 +27,17 @@ async function displayProductsBorrowed(products){
 async function displayProducts(products){
     let htmlString = ""
     products.forEach((product, index) =>{
-      
         htmlString += `\n<div class="col-lg-4 col-md-6 mb-4">
             <div id="${product.pk}" class="card h-100">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <h5 class="card-title mb-0">${product.fields.title}</h5>
+                <div class="card-header d-flex justify-content-center align-items-center" style="text-align: center;">
+                    <h5 class="card-title mb-0" style="color: white; background-color: rgb(8, 169, 8); border-radius: 5px;">AVAILABLE</h5>
                 </div>
-                <div class="card-body rounded-lg">
-					<p class="text-muted small">Id: ${product.pk}</p>
-                    <p class="text-muted small">Penulis: ${product.fields.author}</p>
-                    <p class="text-muted small">Banyak Halaman: ${product.fields.num_pages}</p>
-                    <p class="card-text">${product.fields.description}</p>
+                <div class="card-body rounded-lg" style="text-align:center">
+                    <strong> <p class="card-text">${product.fields.title}</p> </strong>
+                    <strong> <p class="card-text">by: ${product.fields.author}</p> </strong>
                 </div>
                 <div class="card-footer d-flex justify-content-between align-items-center ">
-                    <a href="" style="color: black; text-decoration: none;"> <button onclick=addToCart(${product.pk})>Pinjam</button> </a>
+                    <button onclick=addToCart(${product.pk})>Masukkan Keranjang</button>
                 </div>
             </div>
         </div>`
@@ -52,41 +47,38 @@ async function displayProducts(products){
 async function displayCart(products){
     let htmlString=""
     if (products==null){
-        htmlString=`masih kosong`
+        htmlString=`Belum ada buku nih di keranjang kamu :(`
     }
     else{
-    for(const product of products){
-        const idBook = product.fields.book
-        const idUser = product.fields.user
-        const bookResponse = await fetch(`get-book-data/${idBook}/`);
-        const userResponse = await fetch(`get-user-data/${idUser}/`);
-        const bookData = (await bookResponse.json())[0];
-        const userData = (await userResponse.json())[0];
-        
-        htmlString += `
-        <div style="text-align: center; border: 1px solid black">
-        <br>
-        <tr> <td> ${bookData.fields.title} </td>
-        <td> <a href="" style="color: black; text-decoration: none;"> <button onclick=RemoveFromCart(${product.pk})>Keluarkan</button> </td>
-        </tr>
-        `
-    }
-    htmlString+=`</div>`
-    htmlString+=`<a href="" style="color: black; text-decoration: none;"> <button onclick=addToList()>Pinjam</button>`
+        for(const product of products){
+            const idBook = product.fields.book
+            const idUser = product.fields.user
+            const bookResponse = await fetch(`get-book-data/${idBook}/`);
+            const userResponse = await fetch(`get-user-data/${idUser}/`);
+            const bookData = (await bookResponse.json())[0];
+            const userData = (await userResponse.json())[0];
+            
+            htmlString += `
+            <div style="text-align: center;">
+                <tr style="display: flex; justify-content: space-between;"> 
+                    <td style="color: #003300; font-weight: 600;"> ${bookData.fields.title} </td>
+                    <td> <button class="button-search" onclick=removeFromCart(${product.pk})>Keluarkan</button> </td>
+                </tr>
+            </div>
+            `
+        }
+        htmlString+=`<br><a href="" style="color: black; text-decoration: none;"> <button onclick=addToList()>Pinjam</button>`
     }
     
+    if((htmlString.match(/Keluarkan/g) || []).length !== 0 && (htmlString.match(/Keluarkan/g) || []).length === (document.getElementById("cart").innerHTML.match(/Keluarkan/g) || []).length){
+        alert("Buku sudah ada di keranjang")
+    }
     document.getElementById("cart").innerHTML = htmlString
 }
 
-async function getCart() {
-    return fetch("get-cart/").then((res) => res.json())
-}
-async function getProducts() {
-    return fetch("get-books/").then((res) => res.json())
-}
-async function getProductsBorrowed() {
-    return fetch("get-books-borrowed/").then((res) => res.json())
-}
+async function getCart() { return fetch("get-cart/").then((res) => res.json()) }
+async function getProducts() { return fetch("get-books/").then((res) => res.json()) }
+async function getProductsBorrowed() { return fetch("get-books-borrowed/").then((res) => res.json()) }
 
 async function refreshProducts(){
     const products = await getProducts()
@@ -105,31 +97,22 @@ async function refreshProductsBorrowed(){
     displayProductsBorrowed(products)
 }
 
-function addToCart(id) {
+async function addToCart(id) {
     fetch(`add-to-cart/${id}/`, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', 
-        }
-    })
-    
+    }).then(await getCart()).then(displayCart(await getCart()))
 }
-function RemoveFromCart(id) {
+
+async function removeFromCart(id) {
     fetch(`remove-cart/${id}/`, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', 
-        }
-    })
+    }).then(await getCart()).then(displayCart(await getCart()))
     
 }
 
 function addToList() {
     fetch(`add-to-list/`, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json', 
-        }
     })
     alert("Berhasil Meminjam Buku")
     
